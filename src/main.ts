d13f67fff897a773535e308ea7ba43ccc2f95038
Oleksandra Kuchuhurna <sashyliakych@gmail.com>
2024-05-27 21:00:05 +0300

    add: global exception handling

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 const configService = new ConfigService();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('My API')
+    .setTitle('CodeLions')
     .setDescription('The API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,30 +1,18 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/common/entities/user.entity';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @Get()
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, description: 'Return all users.', type: [User] })
   async getUsers(): Promise<User[]> {
-    try {
-      return await this.usersService.findAllUsers();
-    } catch (error) {
-      throw new HttpException(
-        'Internal Server Error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    return await this.usersService.findAllUsers();
   }
 
   @Post()
@@ -38,13 +26,6 @@ export class UsersController {
     @Body('name') name: string,
     @Body('email') email: string,
   ): Promise<User> {
-    try {
-      return await this.usersService.createUser(name, email);
-    } catch (error) {
-      throw new HttpException(
-        'Internal Server Error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    return await this.usersService.createUser(name, email);
   }
 }
