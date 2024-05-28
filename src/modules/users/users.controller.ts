e4cae67ea994ebc0d2b66e7148b1474228274316
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-05-28 17:34:26 +0300

    docs: updated swagger documentation for register, delete user and get all users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,20 @@
 import { Body, Controller, Get, Post, Delete, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOperation,
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+  ApiBadRequestResponse,
+  ApiResponse,
+  ApiTags,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger';
 import { CreateUserDTO } from './DTO/create.dto';
 import { ResponseUserDTO } from './DTO/response.dto';
+import { ErrorResponse } from 'src/common/ErrorResponse';
+import { responseDescrptions } from 'src/common/responseDescriptions';
 
 @ApiTags('users')
 @Controller('users')
@@ -11,24 +22,66 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Get all users' })
-  @ApiResponse({ status: 200, description: 'Return all users.', type: [User] })
+  @ApiOperation({
+    summary: 'Get all users',
+    tags: ['Users Endpoints'],
+    description: 'This endpoint returns a list of users.',
+  })
+  @ApiOkResponse({
+    description: responseDescrptions.success,
+    type: [User],
+  })
+  @ApiInternalServerErrorResponse({
+    description: responseDescrptions.error,
+    type: ErrorResponse,
+  })
   async getUsers(): Promise<User[]> {
     return await this.usersService.getAllUsers();
   }
 
   @Post()
-  @ApiOperation({ summary: 'Create a new user' })
+  @ApiOperation({
+    summary: 'Create a new user',
+    tags: ['Users Endpoints'],
+    description:
+      'This endpoint creates a new user in the database and returns an object with the new user.',
+  })
   @ApiResponse({
     status: 201,
-    description: 'The user has been successfully created.',
-    type: User,
+    description: responseDescrptions.success,
+    type: ResponseUserDTO,
   })
+  @ApiBadRequestResponse({
+    description: responseDescrptions.error,
+    type: ErrorResponse,
+  })
+  @ApiInternalServerErrorResponse({
+    description: responseDescrptions.error,
+    type: ErrorResponse,
+  })
+  @ApiBody({ type: CreateUserDTO })
   async register(@Body() dto: CreateUserDTO): Promise<ResponseUserDTO> {
     return await this.usersService.registerUser(dto);
   }
 
   @Delete()
+  @ApiOperation({
+    summary: 'Delete a user',
+    tags: ['Users Endpoints'],
+    description: 'This endpoint deletes a user from the database.',
+  })
+  @ApiResponse({
+    status: 204,
+    description: responseDescrptions.success,
+  })
+  @ApiInternalServerErrorResponse({
+    description: responseDescrptions.error,
+    type: ErrorResponse,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the user to delete',
+  })
   async deleteUser(@Param('id') id: string): Promise<void> {
     await this.usersService.deleteUser(id);
   }
