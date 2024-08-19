bb641bc8f0ccde46f4cd03d3027e6a44955f84cb
zweroboy1 <fprognoz@gmail.com>
2024-08-19 20:42:49 +0300

    fix: make public vendor`s endpoints

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -1,12 +1,38 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  ExecutionContext,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
+import { Observable } from 'rxjs';
 import { Errors } from 'src/common/errors';
 
 import { UserResponseDto } from './dto/user-response.dto';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
   handleRequest<TUser = UserResponseDto>(err: Error, user: TUser): TUser {
     if (err || !user) {
       throw new UnauthorizedException(Errors.INVALID_TOKEN);
